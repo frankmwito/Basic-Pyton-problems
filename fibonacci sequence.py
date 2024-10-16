156d11442db4978361f2b79b67f14a095a7138d9
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 20:22:51 +0300

    Fibonacci sequence of an n term

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -1,1 +1,13 @@
 # Fibonacci sequence of an n term
+
+def fibonacci_seq(n):
+    fibonacci_sequence = [0, 1]
+    for i in range(2, n):
+        next_value = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+        fibonacci_sequence.append(next_value)
+    
+    return fibonacci_sequence[:n]
+
+n = int(input("Enter the size of the sequence: "))
+
+print(fibonacci_seq(n))
