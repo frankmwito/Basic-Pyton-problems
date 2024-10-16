8906da0c2f88d800706af4d04235064298bcf886
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 11:31:36 +0300

    max number in a list

--- max in list.py
+++ max in list.py
@@ -1,1 +1,12 @@
 # max number in a list
+
+list_of_numbers = []
+num_of_intergers = int(input("Enter the maximum number of integers: "))
+
+for i in range(num_of_intergers):
+    integers = int(input(f"Enter the {i+1} integer: "))
+    list_of_numbers.append(integers)
+ 
+max_value = max(list_of_numbers)
+
+print(f"The maximum value int the list is: {max_value}")
