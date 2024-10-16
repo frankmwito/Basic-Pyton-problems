fedd98872407adf061bd2696ac4223867b886088
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 11:25:38 +0300

    Factorial calculation

--- Factorial calculation.py
+++ Factorial calculation.py
@@ -1,1 +1,11 @@
 # Factorial calculation
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    print(result)
+        
+
+
+factorial(int(input("Enter the number you wish to find the factorial: ")))
