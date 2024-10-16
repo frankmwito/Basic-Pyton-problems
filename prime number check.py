31c95c35c61a279b39773bca13a3d5b05d3e59f6
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 10:42:59 +0300

    prime number check

--- prime number check.py
+++ prime number check.py
@@ -1,1 +1,15 @@
 # prime number check
+
+def prime_number(num):
+    if num > 1:
+       for i in range(2, num):
+           if num % i == 0:
+               return False
+           else:
+               return True
+    else:
+        print("Number must be greater than 1")
+        return False
+    
+
+print(prime_number(int(input("Enter any number to check if its a prime number: "))))
