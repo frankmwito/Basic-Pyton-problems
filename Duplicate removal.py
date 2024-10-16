2c9a8d96416a443c9406b18857a35d2aa843bebf
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 20:34:21 +0300

    remove duplicates from a list

--- Duplicate removal.py
+++ Duplicate removal.py
@@ -1,2 +1,14 @@
 # remove duplicates from a list
 
+size_of_list = int(input("Enter the size of the list: "))
+numbers_list = []
+
+for i in range(size_of_list):
+    number = int(input(f"Enter the values of list {i+1}: "))
+    numbers_list.append(number)
+    
+sorted_list = sorted(numbers_list)
+
+unique_list = list(set(sorted_list))
+
+print(unique_list)
