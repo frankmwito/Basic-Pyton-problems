83e853327ca4da7ccee2691d69bb8af390503e2f
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 19:54:49 +0300

    anagram checker

--- anagram checker.py
+++ anagram checker.py
@@ -1,1 +1,12 @@
 # anagram checker
+# check if two given words are anagrams of each other
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+new_word1 = word1.lower()
+new_word2 = word2.lower()
+
+if sorted(new_word1) == sorted(new_word2):
+    print("The words are anagrams of each other")
+else:
+    print("The words are  not anagrams of each other")
