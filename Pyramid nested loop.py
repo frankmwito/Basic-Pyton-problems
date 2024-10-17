4020a5314876fcf3cb3867ce77f1c157d80b29e6
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-17 08:36:54 +0300

    Pyramid nested loop

--- Pyramid nested loop.py
+++ Pyramid nested loop.py
@@ -1,1 +1,15 @@
 # Pyramid nested loop
+
+def pyramid_pattern(n):
+    for i in range(1, n+1):
+        # Print spaces
+        for j in range(n-i):
+            print(" ", end="")
+        # Print stars
+        for k in range(2*i-1):
+            print("*", end="")
+        # Move to the next line
+        print()
+
+n = int(input("Enter the number of rows for the pyramid: "))
+pyramid_pattern(n)
