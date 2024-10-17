0946c6d361a11e460bb99ab8973df8236aefbf98
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-17 06:50:29 +0300

    odd numbers of an n term

--- odd numbers of an n term.py
+++ odd numbers of an n term.py
@@ -1,1 +1,15 @@
 # odd numbers of an n term
+
+def odd_numbers(n):
+    odd_list = []
+    
+    for i in range(0, n+1):
+        if i % 2 != 0:
+            odd_list.append(i)
+           
+           
+    print(f"The odd list is: {odd_list}")
+    
+        
+        
+odd_numbers(int(input("Enter the size of odd numbers: ")))
