823d3aa2944096ee08f10e5f588f56cc7a0a46e3
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-17 08:17:03 +0300

    Armstrong number

--- Armstrong number.py
+++ Armstrong number.py
@@ -1,1 +1,19 @@
 # Armstrong number
+
+def is_armstrong(n):
+    armstrong = 0
+    length = len(n)  # Length of the number (number of digits)
+    
+    for digit in n:
+        armstrong += pow(int(digit), length)  # Add each digit raised to the power of the length
+    
+    if int(n) == armstrong:
+        print("The given number is an Armstrong number.")
+        return True
+    else:
+        print("The given number is not an Armstrong number.")
+        return False
+        
+n = input("Enter the number: ")
+
+print(is_armstrong(n))
