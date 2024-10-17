225c67f9b04027423a47a268df350c336fb14553
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-17 07:14:32 +0300

    sum of digits of a given number

--- sum of digits of a given number.py
+++ sum of digits of a given number.py
@@ -1,1 +1,12 @@
 # sum of digits of a given number
+
+n = int(input("Enter the size of list: "))
+num_list = []
+
+for i in range(n):
+    num = int(input(f"Enter the number {i+1}: "))
+    num_list.append(num)
+    
+sum_of_digits = sum(num_list)
+
+print(sum_of_digits)
